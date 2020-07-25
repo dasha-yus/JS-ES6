7475aa6340fe1203d43c9b8ae07330f5ae43b809
Darya Yusipets <DYusipets@gomel.iba.by>
2020-07-25 21:16:11 +0300

    add branch Functions

---
+++ 03 Functions/3_1.js
@@ -0,0 +1,6 @@
+// Minimum
+function min(arg1, arg2){
+	return arg1 < arg2 ? arg1 : arg2;
+}
+console.log(min(0, 10));
+console.log(min(0, -10));
---
+++ 03 Functions/3_3.js
@@ -0,0 +1,15 @@
+// Bean counting
+function countBs(str){
+	return countChar(str, "B");
+}
+function countChar(str, char){
+	var count = 0;
+	for(var i = 0; i < str.length; i++){
+		if(str[i] === char){
+			count++;
+		}
+	}
+	return count;
+}
+console.log(countBs("BBC"));
+console.log(countChar("kakkerlak", "k"));
